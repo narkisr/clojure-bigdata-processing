3c0a190e48bf309cf86049d6cb63535e130fb80c
narkisr <narkisr@gmail.com>
2013-02-10 05:48:07 +0200

    fixing subtitles

--- js/demo-cuepoints.js
+++ js/demo-cuepoints.js
@@ -1,89 +1,19 @@
 	  registerCue(
-	    {state: 'vagrant-demo',
-	     name: 'vagrant',
+	    {state: 'compiless-demo',
+	     name: 'compiless',
 	     slides:{
-			  0:  "Vagrant intro",
-			  18: "Boxes",
-			  61:  "Booting up",
-			  142: "Logging into",
-			  157: "Shared folder",
-			  177: "Halting and destroying"},
-			subs:'vagrant-sub',
-			vid:'vagrant-vid'})
+			  0:  "Our basic tokenize",
+			  8: "Test code",
+			  20:  "Now we eval it",
+			  24: "We get back the result",
+			  33: "Let get rid of !",
+			  50: "We run it again", 
+                    64: "Look! no '!'"},
+			subs:'compiless-sub',
+			vid:'compiless-vid'}
 
 
-	  registerCue(
-	    {state: 'puppet-demo',
-	     name: 'puppet',
-	     slides:{
-                    0: "Puppet demo",
-			  2:  "Checking current status",
-			  15: "Adding Puppet configuration",
-			  28: "Adding a package",
-			  47: "Run during reload",
-			  120: "Adding from within",
-			  142: "Local run"},
-			subs:'puppet-sub',
-			vid:'puppet-vid'})
-
-
-	  registerCue(
-	    {state: 'opsk-demo',
-	     name: 'opsk',
-	     slides:{
-                    0: "Opskeleton demo",
-			  8: "We start by generating a new project",
-			  17: "All the files are created within a new git repo",
-			  27: "Bundler done installing gems ",
-			  32: "Using librarian to install modules",
-			  53: "Booting up the VM",
-			  125: "Hostname is set",
-			  132: "Adding a package", 
-			  157: "Using run.sh from /vagrant"},
-			subs:'opsk-sub',
-		vid:'opsk-vid'})
-
-
-	registerCue(
-	    {state: 'storm-demo',
-	     name: 'storm',
-	     slides:{
-                    0:   "Storm demo",
-			  2:   "Cooking debs",
-			  95:  "Booting up",
-			  142: "Nimbus is up", 
-			  194: "Super_a is up", 
-			  249: "Super_b is up", 
-			  252: "Nimbus ui shows two supervisors"},
-		subs:'storm-sub',
-		vid:'storm-vid'})
-
-
-	 registerCue(
-	    {state: 'packaging-demo',
-	     name: 'packaging',
-	     slides:{
-                   0:  "Packaging demo",
-			 2:  "Current status",
-			 15:  "Cooking in first sandbox",
-			 32:  "Copying package to /vagrant/local-repo",
-			 65: "Installing rpm on puppetizied machine"},
-		subs:'packaging-sub',
-		vid:'packaging-vid'})
-
-
-	registerCue(
-	    {state: 'vagrantdns-demo',
-	     name: 'vagrant dns',
-	     slides:{ 0:  "Vagrand DNS demo",
-                    2:  "Initial state",
-			  10: "Local dns server",
-			  27: "Booting sandboxes",
-			  63: "Registration",
-			  75: "Host ping",
-			  95: "Sandbox ping"}, 
-		subs:'vagrantdns-sub',
-		vid:'vagrantdns-vid'})
+	  )
 
 
 	function speedup(id){
@@ -97,7 +27,7 @@
       var curr_vid;
 	function registerCue(data){
 	  Reveal.addEventListener( data.state, function() {
-	   console.log('starting '+ data.name  +'dns demo');
+	   console.log('starting '+ data.name  +'demo');
 	   curr_vid = data.vid;
 	   cuepoint.pause();
 	   cuepoint.init(data.slides,data.subs,data.vid);
