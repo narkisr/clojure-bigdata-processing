84f8dd7bea6191933a913c3d8abb2607e22aad5e
narkisr <narkisr@gmail.com>
2013-02-13 04:23:44 +0200

    adding fs

--- code/project.clj
+++ code/project.clj
@@ -2,9 +2,10 @@
   :description "Word count demo"
   :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
+                 [fs "1.3.2"]
                  [clojure-hadoop/clojure-hadoop "1.4.1"]
                  [org.clojure/tools.cli "0.2.1"]]
   
-  :plugins [[lein-tarsier "0.9.1"] [perforate "0.2.3"]]
+  :plugins [[lein-tarsier "0.10.0"] [perforate "0.2.3"]]
   :aot [code.core] 
   )
--- code/test/code/core_test.clj
+++ code/test/code/core_test.clj
@@ -1,7 +1,10 @@
 (ns code.core-test
   (:require [clojure.java.io :as io]
             [clojure-hadoop.imports :as imp])
-  (:use [code.core :only (tool-run)] clojure.test))
+  (:use 
+    [fs.core :only (delete-dir)]
+    [clojure.string :only (split-lines)] 
+    [code.core :only (tool-run)] clojure.test))
 
 (imp/import-mapreduce)
 (imp/import-mapreduce-lib)
