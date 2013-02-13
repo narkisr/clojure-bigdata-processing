26bb5595ffefab393c6c921172da987ca656037b
narkisr <narkisr@gmail.com>
2013-02-13 04:42:20 +0200

    adding emr titles

--- js/demo-cuepoints.js
+++ js/demo-cuepoints.js
@@ -11,10 +11,20 @@
                     64: "Look! no '!'"},
 			subs:'compiless-sub',
 			vid:'compiless-vid'}
-
-
-	  )
-
+         );
+       registerCue({state: 'emr-demo',
+	     name: 'emr',
+	     slides:{
+			  0:  "Emr demo",
+			  10: "Job starts",
+                    293: "Ec2 instances running",
+                    472: "Job running",
+                    671: "Job ends", 
+                    716: "S3 result", 
+                    746: "Viewing results"
+                  },
+			subs:'emr-sub',
+			vid:'emr-vid'});
 
 	function speedup(id){
 	  document.getElementById(id).playbackRate += 1.0;
@@ -26,8 +36,11 @@
 
      function jumpTo(id,time){
 	  document.getElementById(id).currentTime = time;
+	 document.getElementById(id).play();
      }
+
       var curr_vid;
+
 	function registerCue(data){
 	  Reveal.addEventListener( data.state, function() {
 	   console.log('starting '+ data.name  +'demo');
